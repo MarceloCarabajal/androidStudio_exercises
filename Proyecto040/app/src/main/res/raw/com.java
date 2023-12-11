1deb39545f289d08e31c2c9a3cfd808d19785998
MarceloCarabajal <marce.tsgia@gmail.com>
2023-12-11 12:08:45 -0300

    Clase MediaPlayer - juego recordar numeros v2

---
+++ Proyecto040/app/src/main/res/raw/com.java
@@ -0,0 +1,15 @@
+package raw;040               E  FATAL EXCEPTION: main
+                                                                                                    Process: com.marce.proytecto040, PID: 5004
+                                                                                                    java.lang.IllegalStateException: Could not find method iniciarJuego(View) in a parent or ancestor Context for android:onClick attribute defined on view class com.google.android.material.button.MaterialButton with id 'button'
+                                                                                                    	at androidx.appcompat.app.AppCompatViewInflater$DeclaredOnClickListener.resolveMethod(AppCompatViewInflater.java:506)
+                                                                                                    	at androidx.appcompat.app.AppCompatViewInflater$DeclaredOnClickListener.onClick(AppCompatViewInflater.java:464)
+                                                                                                    	at android.view.View.performClick(View.java:6256)
+                                                                                                    	at com.google.android.material.button.MaterialButton.performClick(MaterialButton.java:1213)
+                                                                                                    	at android.view.View$PerformClick.run(View.java:24701)
+                                                                                                    	at android.os.Handler.handleCallback(Handler.java:789)
+                                                                                                    	at android.os.Handler.dispatchMessage(Handler.java:98)
+                                                                                                    	at android.os.Looper.loop(Looper.java:164)
+                                                                                                    	at android.app.ActivityThread.main(ActivityThread.java:6541)
+                                                                                                    	at java.lang.reflect.Method.invoke(Native Method)
+                                                                                                    	at com.android.internal.os.Zygote$MethodAndArgsCaller.run(Zygote.java:240)
+                                                                                                    	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:767)
