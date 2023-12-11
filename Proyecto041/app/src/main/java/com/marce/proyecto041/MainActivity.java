e4810756cb19549dc576daba4fbdd3c40ff1ae74
MarceloCarabajal <marce.tsgia@gmail.com>
2023-12-11 11:17:18 -0300

    Intent explicitos (activity propio) e implicitos (activity del disp movil)

---
+++ Proyecto041/app/src/main/java/com/marce/proyecto041/MainActivity.java
@@ -0,0 +1,44 @@
+package com.marce.proyecto041;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirNavegador(View v){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.lanacion.com.ar"));
+        startActivity(i);
+    }
+
+    public void abrirMapa(View v){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:-31.417, -64.183"));
+        startActivity(i);
+    }
+
+    public void mostrarContactos(View v){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("content://contacts/people/"));
+        startActivity(i);
+    }
+    public void abrirCamara(View v){
+        Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(i);
+    }
+    public void abrirMarcadorTel(View v){
+        Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:1234567"));
+        startActivity(i);
+    }
+    public void enviarSms(View v){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:1234567"));
+        startActivity(i);
+    }
+}
