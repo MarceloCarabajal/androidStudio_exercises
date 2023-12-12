afcc867852db74539fe1c9b0d91a1da5da9453f4
MarceloCarabajal <marce.tsgia@gmail.com>
2023-12-12 12:03:04 -0300

    tomar foto mediante intent - guardar en memoria interna su miniatura

---
+++ Proyecto043/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Proyecto043"
+include(":app")
+ 
