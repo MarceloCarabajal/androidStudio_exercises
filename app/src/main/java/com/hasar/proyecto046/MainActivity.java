bd8e27a5b559a69c8f72692f6773e7c28ab7b0d1
MarceloCarabajal <marce.tsgia@gmail.com>
2024-01-30 12:44:41 -0300

    uso de Intent con la clase CreateChooser para que siempre se abra un dialog para compartir un texto por ejemplo

---
+++ app/src/main/java/com/hasar/proyecto046/MainActivity.java
@@ -0,0 +1,35 @@
+package com.hasar.proyecto046;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = findViewById(R.id.et1);
+    }
+
+    public void abrirNavegador(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.google.com.ar"));
+        startActivity(intent);
+    }
+
+    public void compartirTexto(View v){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, et1.getText().toString());
+        Intent eleccion = Intent.createChooser(intent,"Que programa desea utilizar?");
+        startActivity(eleccion);
+    }
+}
