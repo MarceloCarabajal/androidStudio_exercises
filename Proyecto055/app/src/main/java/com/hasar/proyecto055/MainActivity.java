2f45e29543a71139dc7f42685541c8382c9bebe5
MarceloCarabajal <marce.tsgia@gmail.com>
2024-03-07 14:52:06 -0300

    Volley - borrado y modificacion de tablas de mySql

--- Proyecto055/app/src/main/java/com/hasar/proyecto055/MainActivity.java
+++ Proyecto055/app/src/main/java/com/hasar/proyecto055/MainActivity.java
@@ -12,6 +12,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -51,6 +52,84 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, "No existe codigo de articulo ingresado", Toast.LENGTH_SHORT).show();
                             etDescripcion.setText("");
                             etPrecio.setText("");
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        rq.add(requerimiento);
+    }
+
+    public void borrar(View v) {
+        String url = "https://scratchya.com.ar/videosandroidjava/volley/borrar.php";
+        JSONObject parametros = new JSONObject();
+        try {
+            parametros.put("codigo", etCodigo.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest requerimiento = new JsonObjectRequest(Request.Method.POST,
+                url,
+                parametros,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String resu = response.get("resultado").toString();
+                            if(resu.equals("1")){
+                                Toast.makeText(MainActivity.this, "Se elimino el artículo", Toast.LENGTH_SHORT).show();
+                                etDescripcion.setText("");
+                                etCodigo.setText("");
+                                etPrecio.setText("");
+                            } else {
+                                Toast.makeText(MainActivity.this, "No existe codigo de articulo ingresado", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        rq.add(requerimiento);
+    }
+
+    public void modificar(View v) {
+        String url = "https://scratchya.com.ar/videosandroidjava/volley/modificar.php";
+        JSONObject parametros = new JSONObject();
+        try {
+            parametros.put("codigo", etCodigo.getText().toString());
+            parametros.put("descripcion", etDescripcion.getText().toString());
+            parametros.put("precio", etPrecio.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest requerimiento = new JsonObjectRequest(Request.Method.POST,
+                url,
+                parametros,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String resu = response.get("resultado").toString();
+                            if(resu.equals("1")){
+                                Toast.makeText(MainActivity.this, "Se modificaron los datos del artículo", Toast.LENGTH_SHORT).show();
+                                etDescripcion.setText("");
+                                etCodigo.setText("");
+                                etPrecio.setText("");
+                            } else {
+                                Toast.makeText(MainActivity.this, "No existe codigo de articulo ingresado", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
                     }
                 },
